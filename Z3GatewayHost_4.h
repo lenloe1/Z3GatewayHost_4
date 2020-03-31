761818fb2e0c32aab185aa7fd0ece96f13324ed7
lenloe1 <31710001+lenloe1@users.noreply.github.com>
2020-03-31 13:52:46 -0400

    Update Z3GatewayHost_4.h

--- Z3GatewayHost_4.h
+++ Z3GatewayHost_4.h
@@ -342,8 +342,8 @@
 // Use this macro to check if EZSP Common plugin is included
 #define EMBER_AF_PLUGIN_EZSP
 
-// Use this macro to check if EZSP UART plugin is included
-#define EMBER_AF_PLUGIN_EZSP_UART
+// Use this macro to check if EZSP USB plugin is included
+#define EMBER_AF_PLUGIN_EZSP_USB
 
 // Use this macro to check if File Descriptor Dispatch plugin is included
 #define EMBER_AF_PLUGIN_FILE_DESCRIPTOR_DISPATCH
